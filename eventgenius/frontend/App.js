f37192a980d98ee98127785ac366355b18b943ac
khloud54 <kh.t705042000@gmail.com>
2024-05-27 09:01:47 -0700

    ff

---
+++ eventgenius/frontend/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserDashboard from './components/UserDashboard';
+import CreateEvent from './components/CreateEvent';
+
+function App() {
+  return (
+    <div className="App">
+      <UserDashboard />
+      <CreateEvent />
+    </div>
+  );
+}
+
+export default App;
