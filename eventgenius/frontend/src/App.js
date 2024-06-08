9cca6e1019a35cc5f4aca5cc3880f126ed8ab425
khloud54 <138794478+khloud54@users.noreply.github.com>
2024-06-08 20:44:37 +0300

    Update App.js by adding comments

--- eventgenius/frontend/src/App.js
+++ eventgenius/frontend/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import UserDashboard from './components/UserDashboard';
-import CreateEvent from './components/CreateEvent';
+<!--
+  File: App.js
+  Author: Khloud Awis
+  Date: June 8, 2024
+  Description: The React App component orchestrates the rendering of the UserDashboard and CreateEvent components, constituting the fundamental structure of the application.
+-->
+import React from 'react'; // Import React library
+import UserDashboard from './components/UserDashboard'; // Import UserDashboard component
+import CreateEvent from './components/CreateEvent'; // Import CreateEvent component
 
 function App() {
   return (
     <div className="App">
-      <UserDashboard />
-      <CreateEvent />
+      <UserDashboard /> {/* Render UserDashboard component */}
+      <CreateEvent /> {/* Render CreateEvent component */}
     </div>
   );
 }
 
-export default App;
+export default App; // Export the App component as the default export
